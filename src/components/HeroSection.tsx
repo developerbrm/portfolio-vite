16b63525d85fed3b4f0f3bafcb0a743fd0b57dcf
developerbrm <developerbrm@gmail.com>
2025-03-22 16:29:03 +0530

    - HeroSection added

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <div className="h-screen w-full bg-linear-to-br from-blue-500 to-sky-500">
+      <div className="grid place-content-center">
+        <h1 className="text-3xl font-bold text-white">Braham Prakash</h1>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection

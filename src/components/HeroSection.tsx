f77d864a316c67520bd2ab667a6cbbe9f0db15ed
developerbrm <developerbrm@gmail.com>
2025-03-22 20:57:33 +0530

    - hero section changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,19 @@
 const HeroSection = () => {
   return (
-    <div className="h-screen w-full bg-linear-to-br from-blue-500 to-sky-500">
-      <div className="grid place-content-center">
-        <h1 className="text-3xl font-bold text-white">Braham Prakash</h1>
+    <div className="h-screen w-full bg-linear-to-br from-violet-400 to-violet-600">
+      <div className="grid h-full place-content-center text-white">
+        <div className="rounded-md p-2 leading-3 text-nowrap text-white/70">
+          <p className="text-3xl font-medium drop-shadow-md">Hello, I am</p>
+          <h1 className="text-7xl text-white/80">
+            <strong className="drop-shadow-md">Braham Prakash</strong>
+          </h1>
+          <div className="text-3xl">
+            <div className="my-2 drop-shadow-md">
+              an experienced Front-End developer
+            </div>
+            {/* <div className="">and I know how how to do Front-End Stuffs</div> */}
+          </div>
+        </div>
       </div>
     </div>
   )

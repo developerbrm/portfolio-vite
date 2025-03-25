de46c0b63da20f6c69eacf8643aac5f32ecf8e43
developerbrm <developerbrm@gmail.com>
2025-03-25 14:21:09 +0530

    - hero section updates

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="h-screen w-full bg-linear-to-br from-violet-400 to-violet-600">
-      <div className="grid h-full place-content-center text-white">
+      <div className="mx-auto grid h-full max-w-5xl place-content-center text-white">
         <div className="rounded-md p-2 leading-3 text-nowrap text-white/70">
           <p className="text-3xl font-medium drop-shadow-md">Hello, I am</p>
           <h1 className="text-7xl text-white/80">

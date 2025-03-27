519e0c405611e407ef5430d14c0698548f2bb071
developerbrm <developerbrm@gmail.com>
2025-03-27 23:08:51 +0530

    - utilities created

---
+++ src/utilities/utilities.ts
@@ -0,0 +1,30 @@
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa6'
+import { IoDocumentText } from 'react-icons/io5'
+import { MdMail } from 'react-icons/md'
+
+export const SOCIAL_LINKS = [
+  {
+    name: 'GitHub',
+    href: 'https://github.com/developerbrm/',
+    Icon: FaGithub,
+    title: 'GitHub',
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/developerbrm/',
+    Icon: FaLinkedinIn,
+    title: 'LinkedIn',
+  },
+  {
+    name: 'Email',
+    href: 'https://mail.google.com/mail/?view=cm&fs=1&to=developer.brm@gmail.com',
+    Icon: MdMail,
+    title: 'Email',
+  },
+  {
+    name: 'Resume',
+    href: 'https://flowcv.com/resume/fudrr1bjnr/',
+    Icon: IoDocumentText,
+    title: 'Resume',
+  },
+]

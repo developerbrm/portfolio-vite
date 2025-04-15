635e51851a7a97b7f33d10434e79da208250f0af
developerbrm <developerbrm@gmail.com>
2025-04-15 10:52:31 +0530

    - mongo db added

---
+++ server/db/index.ts
@@ -0,0 +1,10 @@
+import { MongoClient } from 'mongodb'
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env'
+  )
+}
+
+export const mongoClient: MongoClient = await MongoClient.connect(MONGODB_URI)

bf51d4632337aef4d269166f37449b4d7d9f4e5a
developerbrm <developerbrm@gmail.com>
2025-04-20 23:36:56 +0530

    - new video added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,7 +5,7 @@ const HERO_SECTION_VIDEO_URL = constructPubicMediaUrl('/hero/hero-video.mp4')
 
 const Hero = () => {
   return (
-    <div className="relative h-screen w-full bg-linear-to-br from-violet-400 to-violet-600">
+    <div className="relative h-screen w-full overflow-hidden">
       <div className="relative z-10 mx-auto grid h-full max-w-5xl place-content-center text-white">
         <div className="rounded-md p-6 leading-3 text-white/70 md:text-nowrap">
           <p className="text-2xl font-medium drop-shadow-md md:text-3xl">
@@ -27,7 +27,7 @@ const Hero = () => {
       </div>
 
       <video
-        className="pointer-events-none absolute inset-0 z-0 h-full w-full object-cover object-center opacity-5"
+        className="pointer-events-none absolute inset-0 z-0 h-full w-full object-cover object-center opacity-20 grayscale-25"
         src={HERO_SECTION_VIDEO_URL}
         autoPlay
         loop
